3dc84219ba89a509f592cbac1b8dcfbfad1909a1
Onur Tekik <otekik@gmail.com>
2022-05-01 14:46:00 +0300

    Add abstract factory tests.

--- Tests/AbstractFactoryTests/AbstrtactFactoryTests.cpp
+++ Tests/AbstractFactoryTests/AbstrtactFactoryTests.cpp
@@ -1,2 +1,34 @@
 #include <AbstractFactory.hpp>
 #include <acutest.h>
+
+void createsProductsTypeX()
+{
+    auto af = new AbstractFactoryX();
+    ProductA* a = af->createProductA();
+    ProductB* b = af->createProductB();
+    TEST_ASSERT(a->getProdAName() == "AX");
+    TEST_ASSERT(b->getProdBName() == "BX");
+
+    delete a;
+    delete b;
+    delete af;
+}
+
+void createsProductsTypeY()
+{
+    auto af = new AbstractFactoryY();
+    ProductA* a = af->createProductA();
+    ProductB* b = af->createProductB();
+    TEST_ASSERT(a->getProdAName() == "AY");
+    TEST_ASSERT(b->getProdBName() == "BY");
+
+    delete a;
+    delete b;
+    delete af;
+}
+
+TEST_LIST = {
+    { "Can create product family X", createsProductsTypeX },
+    { "Can create product family Y", createsProductsTypeY },
+    { NULL, NULL }
+};
